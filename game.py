d2ede3e46c622799b9d874a925be60ec0a846eca
cgrezeszak3 <108580499+cgrezeszak3@users.noreply.github.com>
2022-07-09 11:58:21 -0400

    Add conditional game into python file

--- game.py
+++ game.py
@@ -1,3 +1,33 @@
 # this is the "game.py" file...
 
 print("Rock, Paper, Scissors, Shoot!")
+
+x = input("Please input your play (rock paper or scissors):")
+
+import random
+
+y = random.choice(["rock","paper","scissors"])
+
+print("My Choice =", x)
+print("Computer Choice =", y)
+
+if x == "rock" and y == "rock":
+    print("TIE GAME")
+elif x == "paper" and y == "paper":
+    print("TIE GAME")
+elif x == "scissors" and y == "scissors":
+    print("TIE GAME")
+elif x == "rock" and y == "scissors":
+    print("YOU WIN")
+elif x == "paper" and y == "rock":
+    print ("YOU WIN")
+elif x == "scissors" and y == "paper":
+    print("YOU WIN")
+elif x == "rock" and y == "paper":
+    print("COMPUTER WINS")
+elif x == "scissors" and y == "rock":
+    print("COMPUTER WINS")
+elif x == "paper" and y == "scissors":
+    print("COMPUTER WINS")
+else:
+    print("TRY AGAIN")
